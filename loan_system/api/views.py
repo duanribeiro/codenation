1c2e6deea7d3ce38289ff4b1a3392510ea946b50
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2019-04-20 22:25:48 -0300

    Endpoint "MakeLoan" completo.

--- loan_system/api/views.py
+++ loan_system/api/views.py
@@ -1,32 +1,42 @@
 import json
-from pprint import pprint as pp
+from datetime import timedelta
+from random import randint
 
+from dateutil import parser
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Loan
+
+
+def gera_loan_id():
+    numbers = [str(randint(1000, 9999)) for x in range(4)]
+    return f'{numbers[0][:3]}-{numbers[1]}-{numbers[2]}-{numbers[3]}'
 
 @api_view(['POST'])
 def MakeLoan(request):
     # Recebi as informações do usuário
     payload = json.loads(request.body)
-    pp(payload)
+
+    # Contas financeiras
     r = payload['rate'] / 12
     installment = round((r + r / ((1 + r) ** payload['term'] - 1)) * payload['amount'], 2)
 
-    loan_id = '{:08d}'.format(payload['id'])
-    pp(payload)
+    # Processando os dados
+    date_start = parser.parse(payload['date'])
+    date_end = date_start + timedelta(days=30 * payload['term'])
+    loan_id = gera_loan_id()
 
     # Salvar no banco de dados
-    # loan = Loan(loan_id=,
-    #             amount=,
-    #             rate=,
-    #             date_start=,
-    #             date_end=)
+    loan = Loan(loan_id=loan_id,
+                amount=payload['amount'],
+                rate=payload['rate'],
+                date_start=date_start,
+                date_end=date_end)
+    loan.save()
 
-
-
-    return Response(installment, status=status.HTTP_200_OK)
+    return Response({'loan_id': loan_id, 'installment': installment}, status=status.HTTP_201_CREATED)
 
 
 
