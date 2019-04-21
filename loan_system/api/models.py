acecc2a4ebf7baa0b36e6da6248483d40425a58e
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2019-04-21 14:48:36 -0300

    Todos os endpoint completos.
    Ainda faltam os testes.

--- loan_system/api/models.py
+++ loan_system/api/models.py
@@ -4,20 +4,19 @@ from django.db import models
 class Loan(models.Model):
     loan_id = models.CharField(max_length=18, default=None)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    rate = models.DecimalField(max_digits=3, decimal_places=2)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    installment = models.DecimalField(max_digits=18, decimal_places=2)
     date_start = models.DateField()
     date_end = models.DateField()
 
-
     def __str__(self):
-        return "{}".format(self.id)
+        return "{}".format(self.loan_id)
 
 class Payment(models.Model):
     date = models.DateField()
     type = models.CharField(max_length=4)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    rate = models.DecimalField(max_digits=3, decimal_places=2)
-    loan = models.ForeignKey(Loan, on_delete=models.CASCADE, default=None)
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return "{} - {}".format(self.type, self.amount)
--- loan_system/api/views.py
+++ loan_system/api/views.py
@@ -1,5 +1,6 @@
 import json
 from datetime import timedelta
+from pprint import pprint as pp
 from random import randint
 
 from dateutil import parser
@@ -7,18 +8,19 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Loan
+from .models import Loan, Payment
 
 
-def gera_loan_id():
-    numbers = [str(randint(1000, 9999)) for x in range(4)]
-    return f'{numbers[0][:3]}-{numbers[1]}-{numbers[2]}-{numbers[3]}'
+def random_loan_id():
+    numbers = [str(randint(1000, 9999) + randint(1, 9999)) for x in range(4)]
+    return f'{numbers[0][-3:]}-{numbers[1][-4:]}-{numbers[2][-4:]}-{numbers[3][-4:]}'
+
 
 @api_view(['POST'])
 def MakeLoan(request):
     # Recebi as informações do usuário
     payload = json.loads(request.body)
-
+    print(payload)
     # Contas financeiras
     r = payload['rate'] / 12
     installment = round((r + r / ((1 + r) ** payload['term'] - 1)) * payload['amount'], 2)
@@ -26,12 +28,13 @@ def MakeLoan(request):
     # Processando os dados
     date_start = parser.parse(payload['date'])
     date_end = date_start + timedelta(days=30 * payload['term'])
-    loan_id = gera_loan_id()
+    loan_id = random_loan_id()
 
     # Salvar no banco de dados
     loan = Loan(loan_id=loan_id,
                 amount=payload['amount'],
                 rate=payload['rate'],
+                installment=installment,
                 date_start=date_start,
                 date_end=date_end)
     loan.save()
@@ -39,6 +42,41 @@ def MakeLoan(request):
     return Response({'loan_id': loan_id, 'installment': installment}, status=status.HTTP_201_CREATED)
 
 
+@api_view(['POST'])
+def MakePayment(request, loan_id):
+    # Recebi as informações do usuário
+    payload = json.loads(request.body)
+
+    # Processando os dados
+    date = parser.parse(payload['date'])
+
+    # Salvar no banco de dados
+    payment = Payment(loan_id=Loan.objects.get(loan_id=loan_id),
+                      date=date,
+                      type=payload['payment'],
+                      amount=payload['amount'])
+    payment.save()
+
+    return Response('Ok', status=status.HTTP_201_CREATED)
 
 
+@api_view(['POST'])
+def ListBalance(request, loan_id):
+    # Recebi as informações do usuário
+    payload = json.loads(request.body)
+    date = parser.parse(payload['date'])
+
+    # Pegando o id original do relacionamento
+    loan = Loan.objects.filter(loan_id=loan_id).values('id', 'amount').get()
+
+    # Buscar no banco de dados
+    payments = Payment.objects.filter(loan_id=loan['id']).filter(date__lte=date)
+    for payment in payments:
+        if payment.type == 'made':
+            loan['amount'] -= payment.amount
+        else:
+            loan['amount'] -= (loan.installment - payment.amount)
+        pp(loan['amount'])
+
+    return Response({loan['amount']}, status=status.HTTP_200_OK)
 
